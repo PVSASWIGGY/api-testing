b7c73be4da72380a1f7a270aaf56da28ddd87cfd
PVSASWIGGY <venkata.penugonda@swiggy.in>
2022-07-30 16:37:59 +0530

    added test to create new user

--- app/src/test/java/com/ultralesson/apitesting/AppTest.java
+++ app/src/test/java/com/ultralesson/apitesting/AppTest.java
@@ -6,6 +6,7 @@ package com.ultralesson.apitesting;
 import org.testng.annotations.*;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
 import static org.testng.Assert.*;
 
 public class AppTest {
@@ -16,6 +17,25 @@ public class AppTest {
                 .get("https://gorest.co.in/public/v2/users")
                 .then()
                 .statusCode(200)
+                .log().body();
+    }
+
+    @Test
+    public void testAddNewUser(){
+        given()
+                .accept(JSON)
+                .contentType(JSON)
+                .header("Authorization","Bearer 8ed75a51d163800b750d03c69dc9c1a94c7eedb2c06642ad5c8377904a0d1a91")
+                .body("{\n" +
+                        "    \"name\": \"Tenali Ramakrishna\",\n" +
+                        "    \"gender\": \"male\",\n" +
+                        "    \"email\": \"tenali.ramakrishna@1.com\",\n" +
+                        "    \"status\": \"active\"\n" +
+                        "}")
+                .when()
+                .post("https://gorest.co.in/public/v2/users")
+                .then()
+                .statusCode(201)
                 .log().body();
     }
 }
